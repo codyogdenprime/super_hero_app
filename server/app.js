59db055fe312f490cc0a0f94b868d39611d3f0fa
Cody Ogden <cody@codyogden.com>
2016-09-27 22:37:04 -0500

    Server spin up.

---
+++ server/app.js
@@ -0,0 +1,28 @@
+// Load Path
+const path = require( 'path' );
+// Load express
+const express = require( 'express' );
+const app = express();
+// Load bodyParser
+const bodyParser = require( 'body-parser' );
+app.use( bodyParser.json() );
+// Set Port
+const port = process.env.PORT || 4242;
+
+// Require and setup Mongoose
+const mongoURI = 'mongodb://localhost:27017/co-heroes';
+const mongoose = require('mongoose');
+const MongoDB = mongoose.connect( mongoURI ).connection;
+
+// If there is an error connecting, let us know
+MongoDB.on( 'error', ( err ) => {
+    console.log( 'mongodb connection error:', err);
+});
+
+// Open the connection and start the server listening!
+MongoDB.once( 'open', () => {
+    console.log( 'mongodb is open!' );
+    app.listen( port, () => {
+    	console.log( 'App is running at http://localhost:' + port + '/' );
+    } );
+} );
